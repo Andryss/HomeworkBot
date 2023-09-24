4262a25bfe3a5587297632ff0d01e102b3195bbe
Andryss <andr.krivosheev@gmail.com>
2023-09-24 19:15:15 +0300

    Initial commit

---
+++ src/test/java/ru/andryss/homeworkbot/HomeworkBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.andryss.homeworkbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
