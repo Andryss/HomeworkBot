45ba7dcd935cf01f043d042c394ae3807e680455
Andryss <andr.krivosheev@gmail.com>
2023-09-26 16:39:05 +0300

    Add CommandHandler interface

---
+++ src/main/java/ru/andryss/homeworkbot/commands/CommandHandler.java
@@ -0,0 +1,20 @@
+package ru.andryss.homeworkbot.commands;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.bots.AbsSender;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public interface CommandHandler {
+    CommandInfo getCommandInfo();
+    void onCommandReceived(Update update, AbsSender sender, Runnable onExitHandler) throws TelegramApiException;
+    void onUpdateReceived(Update update) throws TelegramApiException;
+
+    @Data
+    @AllArgsConstructor
+    class CommandInfo {
+        private String name; // starts with '/' (example "/echo")
+        private String description;
+    }
+}
